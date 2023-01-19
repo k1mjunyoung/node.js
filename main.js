6d6a967cc7a288b53c2ba9bb2f92f84f06fc82d2
k1mjunyoung <74641191+k1mjunyoung@users.noreply.github.com>
2023-01-19 16:41:02 +0900

    객체를 이용한 리팩토링

--- main.js
+++ main.js
@@ -3,35 +3,37 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body, control){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    
-    ${body}
-  </body>
-  </html>
-  `;
+// refactoring(tamplate 객체화)
+var template = {
+  html: function(title, list, body, control){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
+    `;
+  },
+  list: function(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i = i + 1;
+    }
+    list = list+'</ul>';
+    return list;
+  }
 }
-function templateList(filelist){
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
-          return list;
-}
- 
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     // 쿼리 값
@@ -42,21 +44,41 @@ var app = http.createServer(function(request,response){
     if(pathname === '/'){
       if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = templateList(filelist); 
-          var template = templateHTML(title, list, `<h2>${title}<h2>${description}`, `<a href="/create">create</a>`);
+          // 프로필 요소들
+          var title = '홍길동';
+          var job = '대학생'
+          var description = '안녕하세요 홍길동입니다.';
+          var youtube = `DhXO4d6m6f4`;
+          var tel = '010-0000-0000'
+          var email = 'example@sch.ac.kr'
+          var address = '순천향대학교 공과대학'
+          var list = template.list(filelist);
+          // Read
+          var html = template.html(title, list,
+            `
+            <h2>${title}</h2>
+            <h3>${job}</h3>
+            </div>${description}</div>
+            </div><iframe width="560" height="315" src="https://www.youtube.com/embed/${youtube}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+            <div>${tel}</div>
+            <div>${email}</div>
+            <div>${address}</div>
+            `,
+            `<a href="/create">create</a>`);
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         })
       // 
       } else {
           fs.readdir('./data', function(error, filelist){
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
               var title = queryData.id;
-              var list = templateList(filelist); 
-              var template = templateHTML(title, list,
-                `<h2>${title}<h2>${description}`,
+              var list = template.list(filelist); 
+              var html = template.html(title, list,
+                `
+                <h2>${title}</h2>
+                <div>${description}</div>
+                `,
                 // 글 생성, 업데이트, 삭제 링크
                 `<a href="/create">create</a>
                 <a href="/update?id=${title}">update</a>
@@ -67,7 +89,7 @@ var app = http.createServer(function(request,response){
                 `
               );
               response.writeHead(200);
-              response.end(template);
+              response.end(html);
             });
           });
         }
@@ -76,8 +98,8 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var youtube = '';
         var address = '';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
+        var list = template.list(filelist);
+        var html = template.html(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p><textarea name="description" placeholder="description"></textarea></p>
@@ -85,7 +107,7 @@ var app = http.createServer(function(request,response){
           </form>
         `, '');
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       })
     } else if(pathname === '/create_process'){
       var body = '';
@@ -108,8 +130,8 @@ var app = http.createServer(function(request,response){
       fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
-          var list = templateList(filelist); 
-          var template = templateHTML(title, list,
+          var list = template.list(filelist); 
+          var html = template.html(title, list,
             `<form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -121,7 +143,7 @@ var app = http.createServer(function(request,response){
             <a href="/update?id=${title}">update</a>`
           );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
     } else if(pathname === '/update_process'){
